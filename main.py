c35d414afb7dd6196939c4224cc54dad28ef8d47
Helen-Mars <503928071@qq.com>
2024-08-13 16:52:24 +0800

    This is a fastapi project.

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ app = FastAPI()
 
 
 # Mount static files
+
+app.mount("/static/header", StaticFiles(directory="static/header"), name="header")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.mount("/static/css", StaticFiles(directory="static/css"), name="css")
 app.mount("/static/js", StaticFiles(directory="static/js"), name="js")
